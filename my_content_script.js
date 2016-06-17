401bee68f3b806aacbe896379bb28f47994b93e9
Sam Grayson <samgrayson@Sams-MacBook-Pro.local>
2016-06-17 11:44:55 -0400

    catch up

--- my_content_script.js
+++ my_content_script.js
@@ -1,14 +1,19 @@
 //document.getElementById('name').innerHTML = "Tony daCosta";
+$(document).ready(function(){
+  $('#name').html('Anthony!!');
 
-$('#name').html('Anthony!!');
+  var googlePromise;
 
-var googlePromise;
+  googlePromise = $.get('https://www.googleapis.com/customsearch/v1?key=AIzaSyDO7iuN79IAgvymA5wdZVk8wSMh1_w2jx4&cx=017576662512468239146:omuauf_lfve&q=lectures');
 
-googlePromise = $.get('https://www.googleapis.com/customsearch/v1?key=AIzaSyDO7iuN79IAgvymA5wdZVk8wSMh1_w2jx4&cx=017576662512468239146:omuauf_lfve&q=lectures');
+  googlePromise.done(function(response){
+    console.log(response);
+  })
+  googlePromise.fail(function(response){
+    console.log('fail', response);
+  })
 
-googlePromise.done(function(response){
-  console.log(response);
-})
-googlePromise.fail(function(response){
-  console.log('fail', response);
-})
+  chrome.runtime.sendMessage({email: "hello"}, function (response) {
+      //console.log(response.farewell);
+  });
+});
--- popup.js
+++ popup.js
@@ -1,8 +1,35 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-    // No tabs or host permissions needed!
-    console.log('Turning ' + tab.url + ' red!');
-    chrome.tabs.executeScript({
-        code: 'document.body.style.backgroundColor="red"'
-    });
-});
+$(document).ready(function(){
+  // Called when the user clicks on the browser action.
+  chrome.browserAction.onClicked.addListener(function(tab) {
+      // No tabs or host permissions needed!
+      console.log('Turning ' + tab.url + ' red!');
+      chrome.tabs.executeScript({
+          code: 'document.body.style.backgroundColor="red"'
+      });
+  });
+
+  chrome.browserAction.onClicked.addListener(function (tab) {
+      // No tabs or host permissions needed!
+
+
+      chrome.tabs.executeScript({
+          code: 'console.log("bang!")'
+      });
+  });
+
+  chrome.runtime.onMessage.addListener(
+      function (request, sender, sendResponse) {
+          console.log(sender.tab ?
+          "from a content script:" + sender.tab.url :
+              "from the extension");
+
+          console.log("foobar");
+          $('#scrape').html('Boo yeah!');
+
+          if (request.email == "hello") {
+              //sendResponse({farewell: "goodbye"});
+
+          }
+
+      });  
+})
